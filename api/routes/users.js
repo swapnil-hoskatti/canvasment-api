292663f8bd03de185bcba0b687e3b43a5e10a443
shoskatt <shoskatt@uncc.edu>
2023-11-06 03:19:23 -0500

    Added jsonwebtoken to Login

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 const User = require("../models/user");
+const user = require("../models/user");
 
 router.get("/", (req, res, next) => {
   User.find()
@@ -36,10 +38,10 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/signup", (req, res, next) => {
-  User.findOne({ email: req.body.email })
+  User.find({ email: req.body.email })
     .exec()
     .then((user) => {
-      if (user) {
+      if (user.length >= 1) {
         console.log(user);
         return res.status(409).json({
           message: "User already exists",
@@ -79,6 +81,44 @@ router.post("/signup", (req, res, next) => {
         });
       }
     });
+});
+
+router.post("/login", (req, res, next) => {
+  User.find({ email: req.body.email })
+  .exec()
+  .then( user => {
+    if (user.length < 1) {
+      return res.status(401).json({
+        message: "Auth failed"
+      });
+    }
+    bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+      if (err) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      if (result) {
+        const token = jwt.sign({
+          email: user[0].email,
+          userId: user[0]._id
+        }, process.env.JWT_KEY, {
+          expiresIn: "1h"
+        })
+        return res.status(200).json({
+          message: "Auth successful",
+          token: token
+        });
+      }
+      res.status(401).json({
+        message: "Auth failed"
+      });
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({ error: err });
+  });
 });
 
 router.get("/:userId", (req, res, next) => {
